618e3eb25f2e4fbc0abcb1a328b1e7da646ec9e3
Irina <balkarovairina@gmail.com>
2021-06-19 10:57:43 +0200

    add: Filter Tasks Functionality

--- app.js
+++ app.js
@@ -52,8 +52,17 @@ function clearTasks() {
     }
 }
 
-function filterTasks() {
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
     
+    document.querySelectorAll('.collection-item').forEach(function(task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
 
 
